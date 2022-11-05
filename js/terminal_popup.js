45425efff4ba04a89f4ebd12f5c5bd7628617618
byel <77223991+ehdzldxn3@users.noreply.github.com>
2022-11-05 20:36:38 +0900

    popup

--- js/terminal_popup.js
+++ js/terminal_popup.js
@@ -6,6 +6,10 @@ document.addEventListener("DOMContentLoaded", () => {
     let log_menu_item = document.querySelectorAll('.log-menu-item')
     let log_desc = document.querySelectorAll('.log-desc')
     let highlight_icon = document.querySelectorAll('.highlight-icon')
+
+    let upload_file_text = document.getElementById('upload_file_text')
+    let upload_file = document.getElementById('upload_file')
+    let upload_file_btn = document.getElementById('upload_file_btn')
     
 
     terminal_menu_item.forEach((item, index) => {
@@ -36,17 +40,25 @@ document.addEventListener("DOMContentLoaded", () => {
         let color = 'red'
         let num = index.toString().slice(0,1)
         
+        
         if(num === '0') color = 'red'
         else if (num === '1') color = 'yellow'
         else if (num === '2') color = 'green'
         else if (num === '3') color = 'blue'
         else if (num === '4') color = 'gray'
+        else color = 'red'
 
         item.classList.add(color)
     })
 
 
+    upload_file_btn.addEventListener('click', (e) => {
+        upload_file.click();
+    })
 
+    upload_file.addEventListener('change', (e) => {
+        upload_file_text.value = upload_file.files[0].name
+    })
 
 
 })
